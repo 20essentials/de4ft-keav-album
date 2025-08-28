2db05a868c1741e5acb88afbbf1ab066f4aa9f17
DominicDev <s0l0estudi4r@gmail.com>
2025-08-28 22:49:28 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'DEAFT KEAV';
+export const nameSongs = [
+	"INVINCIBLE", /*1*/
+	"SAMURAI", /*2*/
+	"FADE AWAY", /*3*/
+	"INVINCIBLE (PIANO VERSION)", /*4*/
+	"PLANES", /*5*/
+	"STARCHILD", /*6*/
+	"SHE GOT ME LIKE ", /*7*/
+	"INVINCIBLE PART II", /*8*/
+	"SAFE & SOUND", /*9*/
+	"DESTINY", /*10*/
+	"SAMURAI (🎹 PIANO VERSION)", /*11*/
+	"BABYLON", /*12*/
+	"FML", /*13*/
+	"NOTHING MORE", /*14*/
+	"FLY AWAY v2", /*15*/
+	"GO BACK", /*16*/
+	"HOUSEFIRE", /*17*/
+	"ALL I NEED", /*18*/
+	"SNAKE EYES", /*19*/
+	"SO SWEET", /*20*/
+]
